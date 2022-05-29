f4a84d4c3f743b2a0aa48d2fbc3a74f0538ed5d6
smart820811 <smart820811@gmail.com>
2022-05-29 16:08:50 +0800

    3.4-批次爬取子網頁的資料

--- scraper.py
+++ scraper.py
@@ -6,4 +6,12 @@ titles = soup.find_all('span',{'class':'lx-stream-post__header-text gs-u-align-m
 title_list=[]
 for title in titles:
     title_list.append(title.getText())
-print(title_list)
+urls = soup.find_all('a',{'class':'qa-heading-link lx-stream-post__header-link'})
+tag_list=[]
+for url in urls:
+    sub_resp = requests.get('https://www.bbc.com'+url.get('href'))
+    sub_soup = BeautifulSoup(sub_resp.text,'lxml')
+    tags = sub_soup.find_all('li',{'class':'bbc-1msyfg1 e1hq59l0'})
+    for tag in tags:
+        tag_list.append(tag.find('a').getText())
+print(tag_list)       
